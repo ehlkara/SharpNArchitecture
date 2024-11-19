e80ab75e5fc4f6853c05125015017e1230db89d9
ehlkara <ehlullah.fb@gmail.com>
2024-11-19 14:48:27 +0300

    global hata yönetimi eklendi.

---
+++ src/corePackages/Core.Packages/Core.CrossCuttingConcerns/Exceptions/ExceptionMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Handlers;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.CrossCuttingConcerns.Exceptions;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly HttpExceptionHandler _exceptionHandler;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+        _exceptionHandler = new HttpExceptionHandler();
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context.Response, ex);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpResponse response, Exception exception)
+    {
+        response.ContentType = "application/json";
+        _exceptionHandler.Response = response;
+        return _exceptionHandler.HandleExceptionAsync(exception);
+    }
+}
